333eba38c66809537fbe768e1b5402c37d40f681
menugget <ningkiling@gmail.com>
2014-08-27 08:49:20 +0200

    Changed name of add.alpha to addAlpha. R filenames have been replaced, and now contain zero version numbers at the end of their filename.

--- R/addAlpha.R
+++ R/addAlpha.R
@@ -1,9 +1,9 @@
-#' @title add.alpha (add alpha channel - transparency - to colors)
+#' @title addAlpha (add alpha channel - transparency - to colors)
 #' @description Takes a vector of colors and adds an alpha channel 
 #' at the given level of transparency.
 #' @export
 
-add.alpha <- function(COLORS, ALPHA){
+addAlpha <- function(COLORS, ALPHA){
 	if(missing(ALPHA)) stop("provide a value for alpha between 0 and 1")
 	RGB <- col2rgb(COLORS, alpha=TRUE)
 	RGB[4,] <- round(RGB[4,]*ALPHA)
